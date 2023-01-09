448be2a11a021abf062d031eb0731b51d66c9cfb
Dhairya Upadhyay <dhairya221b@gmail.com>
2023-01-10 01:28:55 +0530

    try6

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,7 +9,27 @@ const openai = new OpenAIApi(configuration);
 const generateAction = async (req, res) => {
   let baseCompletion;
   if (req.body.app === "Essay"){
-    res.status(200).json({ output: "HEYY Dude!!" });
+
+    const basePromptPrefix = `Write me detailed essay in the style of George Orwell (also include some facts and figures) on the `;
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+    baseCompletion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+      temperature: 0.7,
+      max_tokens: 1400,
+    });
+
+    const basePromptOutput = await baseCompletion.data.choices.pop();
+    
+    while(basePromptOutput === null){
+       setTimeout(() => {
+        console.log("Waiting for completion");
+       }, 1000);
+    }
+
+    res.status(200).json({ output: basePromptOutput });
   }
   else if (req.body.app === "Grammer") {
     const basePromptPrefix = `Correct the grammer and spelling of the given text.\nText: `;
