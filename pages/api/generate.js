e8bd2a662fabc227638bce9efd2c1fc29f348bf7
Dhairya Upadhyay <dhairya221b@gmail.com>
2023-01-09 22:52:07 +0530

    added essay and spell funcitonality status/working

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `Write me detailed essay in the style of George Orwell (also include some facts and figures) on the `;
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 1400,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,122 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [app, setApp] = useState("Essay");
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    if (app === "Essay") {
+      console.log("Calling OpenAI...");
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInput }),
+      });
+
+      const data = await response.json();
+      const { output } = data;
+      console.log("OpenAI replied...", output.text);
+
+      setApiOutput(`${output.text}`);
+    }
+
+    else {
+      console.log("Calling OpenAI...");
+      const response = await fetch("/api/grammerGenerate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInput }),
+      });
+
+      const data = await response.json();
+      const { output } = data;
+      console.log("OpenAI replied...", output.text);
+
+      setApiOutput(`${output.text}`); 
+    }
+
+    setIsGenerating(false);
+  };
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const getPlaceholer = () => {
+    if (app === "Grammer") {
+      return `Input any text...`;
+    } else {
+      return `Title: Artificial intelligence \nDescription: How AI is going to change the world \n`;
+    }
+  };
+
+  const getSubTitle = () => {
+    if (app === "Grammer") {
+      return `Input the text and get the grammer & spelling corrected.`;
+    } else {
+      return `Input the topic and description of essays & get essays as if they were written by George Orwell.`;
+    }
+  };
+
   return (
     <div className="root">
       <Head>
         <title>GPT-3 Writer | buildspace</title>
       </Head>
+      <navbar>
+        <div className="navbar-container">
+          <button className="btn" onClick={() => setApp("Essay")}>
+            Essay Writer
+          </button>
+          <button className="btn" onClick={() => setApp("Grammer")}>
+            Spell Checker
+          </button>
+        </div>
+      </navbar>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Get Ready for {app} Nirvana</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>{getSubTitle()}</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder={getPlaceholer()}
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          ;
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
           </div>
         </div>
       </div>
@@ -30,6 +132,18 @@ const Home = () => {
           </div>
         </a>
       </div>
+      {apiOutput && (
+        <div className="output pb-5" style={{ marginBottom: "20px" }}>
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
